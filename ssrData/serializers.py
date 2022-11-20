108291c4021b55022815b7f6a85b0daf66342988
purush34 <purush3190@gmail.com>
2022-11-21 05:29:51 +0530

    reinittate

--- ssrData/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Project
-
-
-class projectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = ["projectId", "name", "description", "year", "mentor","category"]
